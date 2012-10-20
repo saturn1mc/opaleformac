15341719bfad4b5af78bb2b21f3a124b677631e1
Camille Maurice <saturn1.mc@gmail.com>
2012-10-20 15:55:43 +0200

    Learning CorePlot

--- Opale/OPGrowthGraphView.h
+++ Opale/OPGrowthGraphView.h
@@ -1,5 +1,5 @@
 //
-//  OPGraphTest.h
+//  OPGrowthGraphView.h
 //  Opale
 //
 //  Created by Camille on 06/10/12.
@@ -10,12 +10,32 @@
 
 @interface OPGrowthGraphView : CPTGraphHostingView <CPTPlotSpaceDelegate, CPTPlotDataSource>{
     NSString* title;
-    CPTGraph *graph;
-	NSArray *plotData;
+
+    NSString* sourceFile;
+    NSMutableArray* plotNames;
+	NSMutableArray* plotData;
+    
+    CPTXYGraph* graph;
+    
+    float minX;
+    float minY;
+    float maxX;
+    float maxY;
 }
 
--(void)generateData;
--(void)renderInLayer:(CPTGraphHostingView *)layerHostingView;
+@property (nonatomic, retain) NSString* title;
+@property (nonatomic, retain) NSString* sourceFile;
+@property (nonatomic, retain) NSMutableArray* plotNames;
+@property (nonatomic, retain) NSArray* plotData;
+@property (nonatomic, retain) CPTXYGraph* graph;
+@property (nonatomic) float minX;
+@property (nonatomic) float minY;
+@property (nonatomic) float maxX;
+@property (nonatomic) float maxY;
+
+
+-(void)loadDataFromSource;
+-(void)initGraph;
 -(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot;
 -(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index;
 
