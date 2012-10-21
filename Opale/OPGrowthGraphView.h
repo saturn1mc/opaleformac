e7e1483efaf45221e63c3882d9716cd5418b9ed9
Camille <saturn1.mc@gmail.com>
2012-10-21 19:32:25 +0200

    Ready to implement Coreplot graphs

--- Opale/OPGrowthGraphView.h
+++ Opale/OPGrowthGraphView.h
@@ -8,7 +8,11 @@
 
 #import "CorePlot/CorePlot.h"
 
+@class OPPatient;
+
 @interface OPGrowthGraphView : CPTGraphHostingView <CPTPlotSpaceDelegate, CPTPlotDataSource>{
+    OPPatient* patient;
+    
     NSString* title;
 
     NSString* sourceFile;
@@ -17,25 +21,29 @@
     
     CPTXYGraph* graph;
     
-    float minX;
-    float minY;
-    float maxX;
-    float maxY;
+    float xMin;
+    float yMin;
+    float xMax;
+    float yMax;
 }
 
+@property (nonatomic, retain) OPPatient* patient;
 @property (nonatomic, retain) NSString* title;
 @property (nonatomic, retain) NSString* sourceFile;
 @property (nonatomic, retain) NSMutableArray* plotNames;
-@property (nonatomic, retain) NSArray* plotData;
+@property (nonatomic, retain) NSMutableArray* plotData;
 @property (nonatomic, retain) CPTXYGraph* graph;
-@property (nonatomic) float minX;
-@property (nonatomic) float minY;
-@property (nonatomic) float maxX;
-@property (nonatomic) float maxY;
+@property (nonatomic) float xMin;
+@property (nonatomic) float yMin;
+@property (nonatomic) float xMax;
+@property (nonatomic) float yMax;
 
 
--(void)loadDataFromSource;
+-(void)loadReferenceData;
+-(void)initReferencePlots;
+-(void)initPatientPlots;
 -(void)initGraph;
+-(void)initAxis;
 -(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot;
 -(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index;
 
--- Opale/OPGrowthGraphView.m
+++ Opale/OPGrowthGraphView.m
@@ -9,9 +9,10 @@
 #import "OPGrowthGraphView.h"
 #import "OPView.h"
 #import "CorePlot/CorePlot.h"
+#import "OPPatient.h"
 
 @implementation OPGrowthGraphView
-@synthesize title, sourceFile, plotNames, plotData, graph, minX, minY, maxX, maxY;
+@synthesize patient, title, sourceFile, plotNames, plotData, graph, xMin, yMin, xMax, yMax;
 
 - (void)awakeFromNib{
     sourceFile = @"avg_height";
@@ -20,18 +21,21 @@
     plotData = nil;
     title = nil;
     
-    minX = 0;
-    minY = 0;
-    maxX = 0;
-    maxY = 0;
+    xMin = FLT_MAX;
+    yMin = FLT_MAX;
+    xMax = FLT_MIN;
+    yMax = FLT_MIN;
     
-    [self loadDataFromSource];
+    [self loadReferenceData];
+    
     [self initGraph];
+    [self initReferencePlots];
+    [self initPatientPlots];
+    [self initAxis];
 }
 
 
--(void)loadDataFromSource
-{
+-(void)loadReferenceData{
     
     plotData = [[NSMutableArray alloc] init];
     
@@ -58,12 +62,13 @@
             for(NSString* cell in columns){
                 [data setObject:cell forKey:[plotNames objectAtIndex:j]];
                 
-                if (j == 0){ //x axis value
-                    
-                    maxX = MAX(maxX, [cell floatValue]);
+                if (j == 0){ //Key value (x axis value)
+                    xMin = MIN(xMin, [cell floatValue]);
+                    xMax = MAX(xMax, [cell floatValue]);
                 }
-                else{ //other values
-                    maxY = MAX(maxY, [cell floatValue]);
+                else{ //Other values (y axis value)
+                    yMin = MIN(yMin, [cell floatValue]);
+                    yMax = MAX(yMax, [cell floatValue]);
                 }
                 
                 j++;
@@ -76,43 +81,270 @@
     }
 }
 
--(void)initGraph
-{
-    graph = [[CPTXYGraph alloc] initWithFrame:self.frame];
+-(void)initGraph{
+    graph = [[CPTXYGraph alloc] initWithFrame:self.bounds];
+    [graph applyTheme:[CPTTheme themeNamed:kCPTPlainWhiteTheme]];
     self.hostedGraph = graph;
     
+    title = @"Courbes de croissance";
+    graph.title = title;
+    
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor whiteColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
+    
+    
     CPTXYPlotSpace* plotSpace = (CPTXYPlotSpace*) graph.defaultPlotSpace;
-    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(minX) length:CPTDecimalFromFloat(maxX)];
-    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(minY) length:CPTDecimalFromFloat(maxY)];
+    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(xMin) length:CPTDecimalFromFloat(xMax)];
+    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(0) length:CPTDecimalFromFloat(yMax)];
+}
+
+-(void)initReferencePlots{
     
-    CPTScatterPlot* plot = [[CPTScatterPlot alloc] init];
-    [plot setDataSource:self];
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    NSMutableArray* plots = [[NSMutableArray alloc] init];
     
-    [graph addPlot:plot];
+    for(NSString* pName in plotNames){
+        if([plotNames indexOfObject:pName] > 0){
+            CPTScatterPlot *linePlot = [[CPTScatterPlot alloc] init];
+            linePlot.identifier = pName;
+            linePlot.cachePrecision = CPTPlotCachePrecisionDouble;
+            linePlot.interpolation = CPTScatterPlotInterpolationLinear;
+            
+            linePlot.dataSource = self;
+            linePlot.delegate = self;
+            
+            CPTMutableLineStyle *lineStyle = [linePlot.dataLineStyle mutableCopy];
+            lineStyle.lineWidth = 1.5;
+            lineStyle.lineColor = [CPTColor redColor];
+            linePlot.dataLineStyle = lineStyle;
+            
+            CPTMutableLineStyle *symbolLineStyle = [CPTMutableLineStyle lineStyle];
+            symbolLineStyle.lineColor = [CPTColor redColor];
+            
+            [graph addPlot:linePlot toPlotSpace:plotSpace];
+            [plots addObject:linePlot];
+        }
+    }
+    
+    [plotSpace scaleToFitPlots:plots];
+    
+    CPTMutablePlotRange *xRange = [plotSpace.xRange mutableCopy];
+    [xRange expandRangeByFactor:CPTDecimalFromCGFloat(1.1f)];
+    plotSpace.xRange = xRange;
+    
+    CPTMutablePlotRange *yRange = [plotSpace.yRange mutableCopy];
+    [yRange expandRangeByFactor:CPTDecimalFromCGFloat(1.1f)];
+    
+    plotSpace.yRange = yRange;
+}
+
+-(void)initPatientPlots{
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    
+    CPTScatterPlot *linePlot = [[CPTScatterPlot alloc] init];
+    linePlot.identifier = @"Patient"; //TODO switch with patient name
+    linePlot.cachePrecision = CPTPlotCachePrecisionDouble;
+    linePlot.interpolation = CPTScatterPlotInterpolationLinear;
+    
+    linePlot.dataSource = self;
+    linePlot.delegate = self;
+    
+    CPTMutableLineStyle *lineStyle = [linePlot.dataLineStyle mutableCopy];
+    lineStyle.lineWidth = 2.0f;
+    lineStyle.lineColor = [CPTColor greenColor];
+    linePlot.dataLineStyle = lineStyle;
+    
+    CPTMutableLineStyle *symbolLineStyle = [CPTMutableLineStyle lineStyle];
+    symbolLineStyle.lineColor = [CPTColor greenColor];
+    
+    CPTPlotSymbol *symbol = [CPTPlotSymbol trianglePlotSymbol];
+    symbol.fill = [CPTFill fillWithColor:[CPTColor greenColor]];
+    symbol.lineStyle = symbolLineStyle;
+    symbol.size = CGSizeMake(10.0f, 10.0f);
+    linePlot.plotSymbol = symbol;
+    
+    [graph addPlot:linePlot toPlotSpace:plotSpace];
+}
+
+-(void)initAxis{
+    //Line styles
+    CPTMutableTextStyle *axisTitleStyle = [CPTMutableTextStyle textStyle];
+    axisTitleStyle.color = [CPTColor blackColor];
+    axisTitleStyle.fontName = @"Helvetica-Bold";
+    axisTitleStyle.fontSize = 12.0f;
+    
+    CPTMutableLineStyle *axisLineStyle = [CPTMutableLineStyle lineStyle];
+    axisLineStyle.lineWidth = 2.0f;
+    axisLineStyle.lineColor = [CPTColor blackColor];
+    
+    CPTMutableTextStyle *axisTextStyle = [[CPTMutableTextStyle alloc] init];
+    axisTextStyle.color = [CPTColor blackColor];
+    axisTextStyle.fontName = @"Helvetica-Bold";
+    axisTextStyle.fontSize = 9.0f;
+    
+    CPTMutableLineStyle *tickLineStyle = [CPTMutableLineStyle lineStyle];
+    tickLineStyle.lineColor = [CPTColor blackColor];
+    tickLineStyle.lineWidth = 3.0f;
+    
+    CPTMutableLineStyle *gridLineStyle = [CPTMutableLineStyle lineStyle];
+    gridLineStyle.lineColor = [CPTColor grayColor];
+    gridLineStyle.lineWidth = 0.5f;
+    
+    //X Axis
+    CPTXYAxisSet *axisSet = (CPTXYAxisSet *) graph.axisSet;
+    
+    CPTXYAxis *x = axisSet.xAxis;
+    x.title = @"Age en mois";
+    x.titleTextStyle = axisTitleStyle;
+    x.titleOffset = -20.0f;
+    x.axisLineStyle = axisLineStyle;
+    x.labelingPolicy = CPTAxisLabelingPolicyNone;
+    x.labelTextStyle = axisTextStyle;
+    x.majorTickLineStyle = tickLineStyle;
+    x.minorTickLineStyle = tickLineStyle;
+    x.majorTickLength = 4.0f;
+    x.minorTickLength = 2.0f;
+    x.tickDirection = CPTSignPositive;
+    x.orthogonalCoordinateDecimal = CPTDecimalFromFloat(yMin);
+    
+    CGFloat xCount = [plotData count];
+    NSMutableSet *xLabels = [NSMutableSet setWithCapacity:xCount];
+    NSMutableSet *xLocations = [NSMutableSet setWithCapacity:xCount];
+    
+    for (NSMutableDictionary* data  in plotData) {
+        NSString* strValue = [data objectForKey:[plotNames objectAtIndex:0]];
+        
+        CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:strValue  textStyle:x.labelTextStyle];
+        
+        CGFloat location = [strValue floatValue];
+        label.tickLocation = CPTDecimalFromCGFloat(location);
+        label.offset = -15.0f;
+        
+        if (label) {
+            [xLabels addObject:label];
+            [xLocations addObject:[NSNumber numberWithFloat:location]];
+        }
+    }
+    x.axisLabels = xLabels;
+    x.majorTickLocations = xLocations;
+    
+    //Y Axis
+    CPTXYAxis *y = axisSet.yAxis;
+    y.title = @"Taille en cm";
+    y.titleTextStyle = axisTitleStyle;
+    y.titleOffset = -35.0f;
+    y.axisLineStyle = axisLineStyle;
+    
+    y.majorGridLineStyle = gridLineStyle;
+    y.minorGridLineStyle = gridLineStyle;
+    
+    y.labelingPolicy = CPTAxisLabelingPolicyNone;
+    y.labelTextStyle = axisTextStyle;
+    y.labelOffset = 16.0f;
+    y.majorTickLineStyle = tickLineStyle;
+    y.minorTickLineStyle = tickLineStyle;
+    y.majorTickLength = 4.0f;
+    y.minorTickLength = 2.0f;
+    y.tickDirection = CPTSignPositive;
+    
+    NSInteger majorIncrement = 10;
+    NSInteger minorIncrement = 2;
+    
+    NSMutableSet *yLabels = [NSMutableSet set];
+    NSMutableSet *yMajorLocations = [NSMutableSet set];
+    NSMutableSet *yMinorLocations = [NSMutableSet set];
+    
+    for (NSInteger j = minorIncrement; j <= yMax; j += minorIncrement) {
+        NSUInteger mod = j % majorIncrement;
+        if (mod == 0) {
+            CPTAxisLabel *label = [[CPTAxisLabel alloc] initWithText:[NSString stringWithFormat:@"%li", j] textStyle:y.labelTextStyle];
+            NSDecimal location = CPTDecimalFromInteger(j);
+            label.tickLocation = location;
+            label.offset = -y.majorTickLength - y.labelOffset;
+            if (label) {
+                [yLabels addObject:label];
+            }
+            [yMajorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:location]];
+        } else {
+            [yMinorLocations addObject:[NSDecimalNumber decimalNumberWithDecimal:CPTDecimalFromInteger(j)]];
+        }
+    }
+    
+    y.axisLabels = yLabels;
+    y.majorTickLocations = yMajorLocations;
+    y.minorTickLocations = yMinorLocations;
 }
 
 #pragma mark -
 #pragma mark Plot Data Source Methods
 
--(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
-{
-	return plotData.count;
-}
-
--(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index
-{    
-    NSMutableDictionary* data = [plotData objectAtIndex:index];
-    
-    if(fieldEnum == CPTScatterPlotFieldX){
-        NSString* strValue = (NSString*)[data valueForKey:[plotNames objectAtIndex:0]];
-        NSLog(@"Point %li : x=%f", index, [strValue floatValue]);
-        return [NSNumber numberWithFloat:[strValue floatValue]];
-    }else{
-        NSString* strValue = (NSString*)[data valueForKey:[plotNames objectAtIndex:1]];
-        NSLog(@"Point %li : y=%f", index, [strValue floatValue]);
-        return [NSNumber numberWithFloat:[strValue floatValue]];
+-(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot{
+    if([plot.identifier isEqual:@"Patient"]){
+        //TODO count patient growth points
+        return 3;
+    }
+    else{
+        return plotData.count;
     }
 }
 
+-(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index{
+    
+    if([plot.identifier isEqual:@"Patient"]){
+        
+        //TODO use patient history
+        
+        if(fieldEnum == CPTScatterPlotFieldX){
+            switch (index){
+            case 0:
+                return [NSDecimalNumber numberWithFloat:0];
+            case 1:
+                return [NSDecimalNumber numberWithFloat:10];
+            case 2:
+                return [NSDecimalNumber numberWithFloat:20];
+            default:
+                return [NSDecimalNumber numberWithFloat:0];
+            }
+        }
+        else{
+            switch (index){
+                case 0:
+                    return [NSDecimalNumber numberWithFloat:48];
+                case 1:
+                    return [NSDecimalNumber numberWithFloat:70];
+                case 2:
+                    return [NSDecimalNumber numberWithFloat:80];
+                default:
+                    return [NSDecimalNumber numberWithFloat:0];
+            }
+        }
+    
+    }
+    else{ //Reference plots
+        if(fieldEnum == CPTScatterPlotFieldX){
+            NSMutableDictionary* data = [plotData objectAtIndex:index];
+            NSString* strValue = [data objectForKey:[plotNames objectAtIndex:0]];
+            
+            if(strValue){
+                return [NSDecimalNumber numberWithFloat:[strValue floatValue]];
+            }
+        }
+        else if(fieldEnum == CPTScatterPlotFieldY){
+            NSMutableDictionary* data = [plotData objectAtIndex:index];
+            NSString* strValue = [data objectForKey:plot.identifier];
+            
+            if(strValue){
+                return [NSDecimalNumber numberWithFloat:[strValue floatValue]];
+            }
+        }
+    }
+    
+    return [NSDecimalNumber zero];
+}
 
 @end
