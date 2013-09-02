46de9479307549779b9525855bea0625ff1922e1
Camille <saturn1.mc@gmail.com>
2013-09-02 22:38:35 +0200

    Child patient view

---
+++ Opale/OPChildPatientView.h
@@ -0,0 +1,13 @@
+//
+//  OPChildPatientView.h
+//  Opale
+//
+//  Created by Camille on 02/09/13.
+//
+//
+
+#import "OPExistingPatientView.h"
+
+@interface OPChildPatientView : OPExistingPatientView
+
+@end
---
+++ Opale/OPChildPatientView.m
@@ -0,0 +1,13 @@
+//
+//  OPChildPatientView.m
+//  Opale
+//
+//  Created by Camille on 02/09/13.
+//
+//
+
+#import "OPChildPatientView.h"
+
+@implementation OPChildPatientView
+
+@end
